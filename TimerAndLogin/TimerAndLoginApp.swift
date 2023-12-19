77a14edbeda0dec50efeeac70303aaa245d1b340
Gridenko Maks <Maksgridenko@ya.ru>
2023-12-19 16:59:44 +0300

    Initial Commit

---
+++ TimerAndLogin/TimerAndLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAndLoginApp.swift
+//  TimerAndLogin
+//
+//  Created by Maxim Gridenko on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TimerAndLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
